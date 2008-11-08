8b08e623dbce3c89726fef042f79983c1fa27a9c
biegleux <biegleux@3fc2e0a0-1f4e-48c8-a278-2838eeee259d>
2008-11-08 15:04:10 +0000

    Initial import.

---
+++ trunk/playerList.class.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Defines PlayerList class.
+ *
+ * @package recAnalyst
+ * @version $Id$
+ * @author biegleux <biegleux@gmail.com>
+ * @copyright copyright (c) 2008 biegleux
+ * @license http://www.opensource.org/licenses/gpl-3.0.html GNU General Public License version 3 (GPLv3)
+ * @link http://recanalyst.sourceforge.net/
+ * @filesource
+ */
+
+/**
+ * PlayerList class
+ *
+ * PlayerList implements list of players in a game.
+ *
+ * @package recAnalyst
+ */
+class PlayerList extends ParentList
+{
+	/**
+	 * Adds a player to the list.
+	 *
+	 * @param Player $player the player we wish to add
+	 */
+	public function addPlayer (Player $player)
+	{
+		parent::addItem ($player);
+	}
+
+	/**
+	 * Returns a player at the specified offset.
+	 *
+	 * @param int an index of the player
+	 * @return Player|bool the player at the index or false if the index is out of the range
+	 */
+	public function getPlayer ($index)
+	{
+		return parent::getItem ($index);
+	}
+
+	/**
+	 * Returns a player with the index property equal to the one defined.
+	 *
+	 * @param int $index player's index
+	 * @return Player|bool false if no player has been found
+	 */
+	public function getPlayerByIndex ($index)
+	{
+		for ($i = 0; $i < $this->count; $i++)
+		{
+			if ($this->list[$i]->index == $index)
+			{
+				return $this->list[$i];
+			}
+		}
+		return false;
+	}
+}
+?>
